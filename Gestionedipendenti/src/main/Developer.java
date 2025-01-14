9149cfbadcac7316829a86bdcae11ded2ac82c3c
Edoardo <edoardoderosaa@gmail.com>
2025-01-14 17:34:20 +0100

    Rimozione errori

--- Gestionedipendenti/src/main/Developer.java
+++ Gestionedipendenti/src/main/Developer.java
@@ -7,8 +7,6 @@ public class Developer extends Employee {
 	// INIZIALIZZIAMO IL COSTRUTTORE
 	public Developer(int id, String nome, String cognome, double stipendioBase, String linguaggiConosciuti, String progettiAssegnati) {
 		super(id, nome, cognome, stipendioBase);
-		this.linguaggiConosciuti = linguaggiConosciuti;
-		this.progettiAssegnati = progettiAssegnati;
 
 	}
 
