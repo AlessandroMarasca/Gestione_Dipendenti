e4aced070ee900de266f8df869cf189949571db1
Marco Pelissero <pelisseromarco58@gmail.com>
2025-01-17 15:43:06 +0100

    modificato costruttore

--- Gestionedipendenti/src/main/Team.java
+++ Gestionedipendenti/src/main/Team.java
@@ -6,14 +6,12 @@ import java.util.Scanner;
 public class Team {
 	int id_team;
 	String nome;
-	int id_progetto;
-	int id_manager;
 	
-	public Team(int id_team,String nome,int id_progetto, int id_manager) {
+	
+	public Team(int id_team,String nome) {
 		this.id_team=id_team;
 		this.nome=nome;
-		this.id_progetto=id_progetto;
-		this.id_manager=id_manager;
+		
 	}
 	
 	/**
